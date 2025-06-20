9eedd91c36a1ba2d541ade6d6d54fd3e426df4dc
warzes <warzes@mail.ru>
2025-06-20 16:47:51 +1000

    ссылки

--- src/Game/GameApp.cpp
+++ src/Game/GameApp.cpp
@@ -1,6 +1,44 @@
 ﻿#include "stdafx.h"
 #include "GameApp.h"
 
+https://github.com/potato3d/glbase
+https://github.com/potato3d/instancing
+https://github.com/potato3d/modern-opengl
+https://github.com/potato3d/azdo
+https://github.com/potato3d?tab=repositories
+https://github.com/nlguillemot?tab=repositories
+https://github.com/nfrechette/rtm  - это
+
+https://github.com/Ilya3point999K/RAL
+https://github.com/bioglaze/rasterizer
+https://github.com/elnormous/SoftwareRenderer/tree/master
+https://github.com/cadenji/foolrenderer
+https://github.com/LogicEu?tab=repositories
+https://github.com/maxbrundev/Rasterizer - это
+https://github.com/ssloy/tinyraytracer/wiki/Part-1:-understandable-raytracing
+https://github.com/ssloy/tinyrenderer/wiki
+
+https://www.youtube.com/watch?v=L4HEU8KOoCA
+
+https://www.youtube.com/watch?v=yyJ-hdISgnw&t=205s
+https://www.youtube.com/watch?v=kcBCk2E3cYQ
+https://github.com/Marzac/le3d
+https://github.com/andrew-lim/sw3dcpp
+https://github.com/DCubix/3D-Software-Renderer/blob/master/src/main.cpp
+https://github.com/potato3d/tess/blob/main/geometries.h
+https://github.com/rowanlovejoy/bounding-box
+
+https://github.com/polymonster/maths
+
+https://github.com/iboB/picobench
+
+
+
+https://github.com/bytecode77/fastpix3d
+https://www.youtube.com/watch?v=PmrtuQ0omI8
+
+https://www.youtube.com/watch?v=Ttw6l1-5Vu4
+
 
 
 //=============================================================================
